876ba750b5374eb65bab2689726df6473d09b83c
LUCA <luca.fernandez17@gmail.com>
2019-01-01 18:46:24 +0100

    offline page

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -3,6 +3,7 @@
 var doCache = true;
 
 var CACHE_NAME = 'pwa-app-cache';
+var cacheVersion = 1;
 
 // Delete old caches
 self.addEventListener('activate', event => {
@@ -20,23 +21,26 @@ self.addEventListener('activate', event => {
 });
 
 // This triggers when user starts the app
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   if (doCache) {
     event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
+      caches.open(CACHE_NAME + cacheVersion)
+        .then(function (cache) {
           fetch('asset-manifest.json')
             .then(response => {
-              response.json();
+              console.log("waitUntil response ", response);
+              return response.json();
             })
             .then(assets => {
               // We will cache initial page and the main.js
               // We could also cache assets like CSS and images
               const urlsToCache = [
                 '/',
-                assets['main.js']
+                assets['main.js'],
+                'offline.html'
               ];
-              cache.addAll(urlsToCache);
+              console.log("cached urls: ", urlsToCache);
+              return cache.addAll(urlsToCache);
             })
         })
     );
@@ -44,12 +48,26 @@ self.addEventListener('install', function(event) {
 });
 
 // Here we intercept request and serve up the matching files
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   if (doCache) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
+    // request.mode = navigate isn't supported in all browsers
+    // so include a check for Accept: text/html header.
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+      event.respondWith(
+        fetch(event.request.url).catch(error => {
+          // Return the offline page
+          return caches.match('offline.html');
+        })
+      );
+    }
+    else {
+      // Respond with everything else if we can
+      event.respondWith(caches.match(event.request)
+        .then(function (response) {
+          return response || fetch(event.request);
+        })
+      );
+    }
+
   }
 });
