87fc736472da859f0604434c0902bad6a881b63a
LUCA <luca.fernandez17@gmail.com>
2019-01-02 13:30:34 +0100

    firebase dashboard

--- client/src/App/components/Dashboard.js
+++ client/src/App/components/Dashboard.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import * as firebase from 'firebase';
+import ls from "local-storage";
+import Game from "./Game";
+import "./Game.scss";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBvxVAYOPwEk2ApbU_DfQ-tmcgOfJ6k4Y",
+  authDomain: "ionicvsk.firebaseapp.com",
+  databaseURL: "https://ionicvsk.firebaseio.com",
+  projectId: "ionicvsk",
+  storageBucket: "",
+  messagingSenderId: "589403062376"
+};
 
 class Dashboard extends React.Component {
+
+  componentDidMount() {
+    this.setState({ games: [] });
+    var user = ls.get("user");
+    if (user !== null) {
+      var app = firebase.initializeApp(firebaseConfig);
+
+      this.db = app.database();
+
+      //var games = this.db.ref("/" + user.googleId + "/games");
+      var games = this.db.ref("users/SnTg4iqWQ4WnwFkIDhh7WmtTHFo2/games");
+      games.on("value", snapshot => {
+        console.log(snapshot.val());
+
+        var data_list = [];
+        snapshot.forEach(function (childSnapshot) {
+          var childData = childSnapshot.val();
+          data_list.push(childData);
+        });
+        console.log("data_list", data_list);
+        this.setState({ games: data_list });
+      })
+    }
+  }
 
   render() {
     return (
       <div>
-          Dashboard
+        {
+          this.state != null?
+            this.state.games.map(game => {
+            return (<Game key={game.id} game={game}/>)
+          })
+          : ""
+        }
       </div>
     );
   }
---
+++ client/src/App/components/Game.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const styles = {
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+};
+
+function Game(props) {
+    const { classes } = props;
+
+    function openGame() {
+        console.log("open game " + props.game.id);
+    }
+
+    return (
+        <Card className={classes.card + " game-card"}>
+            <CardContent>
+                <div className="game-content" click={openGame()}>
+                    <div className={"float-left side-text live-text" + (props.game.live ? 'live' : '')}>{props.game.live ? 'Live' : 'Punteggio finale'}</div>
+                    <div className="float-right side-text">{props.game.date !== undefined ? props.game.date.day : ""}</div>
+                    <div className="clear">
+                        <div className={"float-left relevant-text" + (props.game.resultA > props.game.resultB ? 'winner' : "")}>{props.game.teamA}</div>
+                        <div className={"float-right relevant-text" + (props.game.resultA > props.game.resultB ? "winner" : "")}>{props.game.resultA}</div>
+                    </div>
+                    <div className="clear">
+                        <div className={"float-left relevant-text" + (props.game.resultB > props.game.resultA ? "winner" : "")}>{props.game.teamB}</div>
+                        <div className={"float-right relevant-text" + (props.game.resultB > props.game.resultA ? "winner" : "")}>{props.game.resultB}</div>
+                    </div>
+                    <div className="clear side-text">{props.game.location.name}</div>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
+
+Game.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(Game);
