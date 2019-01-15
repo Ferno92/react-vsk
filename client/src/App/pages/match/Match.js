87d44739c39b2d7892f2a8bd2f6a0d4a027f8f17
Luca Fernandez <luca.fernandez@spot.it>
2019-01-15 11:35:30 +0100

    moved match info on a new page

--- client/src/App/pages/match/Match.js
+++ client/src/App/pages/match/Match.js
@@ -4,7 +4,6 @@ import { withStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import ArrowBack from "@material-ui/icons/ArrowBack";
-import Undo from "@material-ui/icons/Undo";
 import Button from "@material-ui/core/Button";
 import Delete from "@material-ui/icons/Delete";
 import Share from "@material-ui/icons/Share";
@@ -20,11 +19,6 @@ import "firebase/database";
 import ls from "local-storage";
 import { firebaseConfig } from "../../App";
 import "./Match.scss";
-import ResultButton from "../../components/resultButton/ResultButton";
-import SetHistoryDialog from "../../components/SetHistoryDialog";
-import ListItem from "@material-ui/core/ListItem";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -33,6 +27,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import MatchInfo from "../match-info/MatchInfo";
 
 const styles = theme => ({
   header: {
@@ -74,10 +69,6 @@ class Match extends React.Component {
   state = {
     value: 0,
     currentGame: null,
-    selectedSet: {
-      open: false,
-      history: []
-    },
     confirmDialogOpen: false,
     spectator: false,
     anchorEl: null
@@ -111,7 +102,7 @@ class Match extends React.Component {
         ...prevState,
         currentGame: snapshot.val()
       }));
-      // console.log(this.state.currentGame);
+      console.log("game from db", this.state.currentGame);
     });
   }
 
@@ -130,133 +121,8 @@ class Match extends React.Component {
     this.props.history.push("/");
   }
 
-  add(team) {
-    var setsMissing = {
-      a: team === "resultA" ? 1 : 0,
-      b: team === "resultA" ? 0 : 1,
-      history: []
-    };
-    var currentGame = {
-      ...this.state.currentGame,
-      sets: this.state.currentGame.sets
-        ? {
-          ...this.state.currentGame.sets,
-          [this.state.currentGame.sets.length - 1]: {
-            ...this.state.currentGame.sets[
-            this.state.currentGame.sets.length - 1
-            ],
-            [team === "resultA" ? "a" : "b"]:
-              this.state.currentGame.sets[
-              this.state.currentGame.sets.length - 1
-              ][team === "resultA" ? "a" : "b"] + 1
-          }
-        }
-        : [setsMissing]
-    };
-    if (currentGame.sets[Object.keys(currentGame.sets).length - 1].history !== undefined) {
-      currentGame.sets[Object.keys(currentGame.sets).length - 1].history.push(team === "resultA" ? { a: "x", b: "-" } : { a: "-", b: "x" });
-    } else {
-      currentGame.sets[Object.keys(currentGame.sets).length - 1].history = [team === "resultA" ? { a: "x", b: "-" } : { a: "-", b: "x" }];
-    }
-
-    if ((currentGame.sets[
-      Object.keys(currentGame.sets).length - 1
-    ].a >= 25 || currentGame.sets[
-      Object.keys(currentGame.sets).length - 1
-    ].b >= 25) && Math.abs(currentGame.sets[
-      Object.keys(currentGame.sets).length - 1
-    ].a - currentGame.sets[
-      Object.keys(currentGame.sets).length - 1
-    ].b) >= 2) {
-      // the set is over
-      currentGame[team]++;
-
-      if (currentGame.resultA === 3 || currentGame.resultB === 3) {
-        currentGame.live = false;
-      } else {
-
-        currentGame.sets[Object.keys(currentGame.sets).length] = { a: 0, b: 0 };
-      }
-    }
-    this.gameRef.update(currentGame);
-  }
-
-  remove(team) {
-    var setsMissing = {
-      a: 0,
-      b: 0
-    };
-    var currentGame = {
-      ...this.state.currentGame,
-      sets: this.state.currentGame.sets
-        ? {
-          ...this.state.currentGame.sets,
-          [this.state.currentGame.sets.length - 1]: {
-            ...this.state.currentGame.sets[
-            this.state.currentGame.sets.length - 1
-            ],
-            [team === "resultA" ? "a" : "b"]:
-              this.state.currentGame.sets[
-                this.state.currentGame.sets.length - 1
-              ][team === "resultA" ? "a" : "b"] > 0 ?
-                this.state.currentGame.sets[
-                this.state.currentGame.sets.length - 1
-                ][team === "resultA" ? "a" : "b"] - 1 : 0
-          }
-        }
-        : [setsMissing]
-    };
-    this.removeLastHistoryFor(currentGame, team);
-
-    this.gameRef.update(currentGame);
-  }
-
-  removeLastHistoryFor(currentGame, team) {
-    if (currentGame.sets[Object.keys(currentGame.sets).length - 1].history !== undefined) {
-      var foundIndex = -1;
-      for (var i = currentGame.sets[Object.keys(currentGame.sets).length - 1].history.length - 1; i >= 0; i--) {
-        if ((team === "resultA" && currentGame.sets[Object.keys(currentGame.sets).length - 1].history[i].a === "x") ||
-          (team === "resultB" && currentGame.sets[Object.keys(currentGame.sets).length - 1].history[i].b === "x")) {
-          foundIndex = i;
-        }
-      }
-      if (foundIndex > -1) {
-        currentGame.sets[Object.keys(currentGame.sets).length - 1].history.splice(foundIndex, 1);
-      }
-    }
-  }
-
-  undo() {
-    var team = "";
-    var currentGame = {
-      ...this.state.currentGame
-    };
-    currentGame.sets.splice(currentGame.sets.length - 1, 1);
-    if (currentGame.sets[currentGame.sets.length - 1].a > currentGame.sets[currentGame.sets.length - 1].b) {
-      currentGame.sets[currentGame.sets.length - 1].a--;
-      currentGame.resultA--;
-      team = "resultA";
-    } else {
-      currentGame.sets[currentGame.sets.length - 1].b--;
-      currentGame.resultB--;
-      team = "resultB";
-    }
-    this.removeLastHistoryFor(currentGame, team);
-    this.gameRef.update(currentGame);
-  }
-
   deleteGame() {
     this.setState({ ...this.state, confirmDialogOpen: true });
-  }
-
-  closeHistory(set) {
-    this.setState({ ...this.state, selectedSet: { ...set, open: false } });
-  }
-
-  openHistory(set) {
-    if (set.history !== undefined) {
-      this.setState({ ...this.state, selectedSet: { ...set, open: true } });
-    }
   }
 
   handleCloseConfirmDialog(toDelete) {
@@ -280,10 +146,10 @@ class Match extends React.Component {
     var url = window.location.origin;
     if (this.state.spectator) {
       // same url
-      url += this.props.pathname;
+      url += this.props.match.url;
     } else {
       // url += this.props.pathname + "/" + ls.get("user").googleId;
-      url += this.props.pathname + "/SnTg4iqWQ4WnwFkIDhh7WmtTHFo2";
+      url += this.props.match.url + "/SnTg4iqWQ4WnwFkIDhh7WmtTHFo2";
     }
     var newVariable = window.navigator;
     if (newVariable && newVariable.share) {
@@ -296,7 +162,7 @@ class Match extends React.Component {
         .then(() => console.log("Successful share"))
         .catch(error => console.log("Error sharing", error));
     } else {
-      alert("share not supported");
+      alert("share not supported for: " + url);
     }
   }
 
@@ -339,18 +205,16 @@ class Match extends React.Component {
             >
               {!this.state.spectator && (
                 <MenuItem key={"deleteGame"} onClick={this.deleteGame.bind(this)}>
+                <Delete />
                   Elimina
-              <Delete />
                 </MenuItem>
               )}
 
               <MenuItem key={"shareGame"} onClick={this.share.bind(this)}>
-                Condividi
               <Share />
+                Condividi
               </MenuItem>
             </Menu>
-
-
           </Toolbar>
           <Tabs
             value={this.state.value}
@@ -380,127 +244,8 @@ class Match extends React.Component {
           className="tab-container"
         >
           <TabContainer dir={theme.direction}>
-            {this.state.currentGame && this.state.currentGame.live && (
-              <div className="flex-container set-info">
-                <div className="flex-child-bigger">
-                  {this.state.currentGame ? this.state.currentGame.resultA : ""}
-                </div>
-                <div className="flex-child separator">-</div>
-
-                <div className="flex-child-bigger">
-                  {this.state.currentGame ? this.state.currentGame.resultB : ""}
-                </div>
-              </div>
-            )}
-
-            <div className="flex-container">
-              <div className="flex-child">
-                <p className="team-name">
-                  {this.state.currentGame ? this.state.currentGame.teamA : ""}
-                </p>
-                <ResultButton
-                  result={
-                    this.state.currentGame
-                      ? this.state.currentGame.live ? this.state.currentGame.sets
-                        ? this.state.currentGame.sets[
-                          this.state.currentGame.sets.length - 1
-                        ].a
-                        : 0
-                        : this.state.currentGame.resultA
-                      : 0
-                  }
-                  team={0}
-                  add={this.add.bind(this, "resultA")}
-                  remove={this.remove.bind(this, "resultA")}
-                  disabled={this.state.currentGame ? !this.state.currentGame.live : true}
-                  spectator={this.state.spectator}
-                />
-              </div>
-              <div className="flex-child">
-                <p className="team-name">
-                  {this.state.currentGame ? this.state.currentGame.teamB : ""}
-                </p>
-                <ResultButton
-                  result={
-                    this.state.currentGame
-                      ?
-                      this.state.currentGame.live ?
-                        this.state.currentGame.sets
-                          ? this.state.currentGame.sets[
-                            this.state.currentGame.sets.length - 1
-                          ].b
-                          : 0
-                        : this.state.currentGame.resultB
-                      : 0
-                  }
-                  team={1}
-                  add={this.add.bind(this, "resultB")}
-                  remove={this.remove.bind(this, "resultB")}
-                  disabled={this.state.currentGame ? !this.state.currentGame.live : true}
-                  spectator={this.state.spectator}
-                />
-              </div>
-            </div>
-            <div className="undo-container">
-              {this.state.currentGame !== null && this.state.currentGame.live && (this.state.currentGame.resultA !== 0 || this.state.currentGame.resultB !== 0) &&
-                this.state.currentGame.sets[this.state.currentGame.sets.length - 1].a === 0 && this.state.currentGame.sets[this.state.currentGame.sets.length - 1].b === 0 && (
-                  <Button variant="contained" color="primary" className={classes.button} onClick={this.undo.bind(this)}>
-                    <Undo></Undo>
-                    Back
-            </Button>)}
-            </div>
-            <div className="set-list">
-              <List>
-                {this.state.currentGame && this.state.currentGame.sets
-                  ? this.state.currentGame.sets.map((set, index) => {
-                    var inCorso = (
-                      <div key={index}>
-                        <ListItem button style={{ display: "block" }} onClick={this.openHistory.bind(this, set)}>
-                          <div className="set-title">
-                            {index + 1 + "° SET: "}
-                          </div>
-                          <div className="flex-container">
-                            <div className={"flex-child"}>In corso...</div>
-                          </div>
-                        </ListItem>
-                        <Divider />
-                      </div>
-                    );
-                    return index === (this.state.currentGame ? this.state.currentGame.sets.length - 1 : 0) && this.state.currentGame.live ? (
-                      inCorso
-                    ) : (
-                        <div key={index}>
-                          <ListItem button style={{ display: "block" }} onClick={this.openHistory.bind(this, set)}>
-                            <div className="set-title">
-                              {index + 1 + "° SET: "}
-                            </div>
-                            <div className="flex-container">
-                              <div
-                                className={
-                                  "flex-child " + (set.a > set.b ? "bold" : "")
-                                }
-                              >
-                                {set.a}
-                              </div>
-                              <div
-                                className={
-                                  "flex-child " + (set.b > set.a ? "bold" : "")
-                                }
-                              >
-                                {set.b}
-                              </div>
-                            </div>
-                          </ListItem>
-                          <Divider />
-                        </div>
-                      );
-                  })
-                  : ""}
-              </List>
-            </div>
-
-            <SetHistoryDialog history={this.state.selectedSet.history} open={this.state.selectedSet.open !== undefined && this.state.selectedSet.open} close={this.closeHistory.bind(this, this.state.selectedSet)} />
-          </TabContainer>
+                <MatchInfo currentGame={this.state.currentGame} spectator={this.state.spectator} gameRef={this.gameRef}/>
+            </TabContainer>
           <TabContainer dir={theme.direction}>Item Two</TabContainer>
           <TabContainer dir={theme.direction}>Item Three</TabContainer>
         </SwipeableViews>
