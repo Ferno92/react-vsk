cdfc4a8461ce711ac92550ebeb384b82d0ee2960
Luca Fernandez <luca.fernandez@spot.it>
2019-03-14 12:19:42 +0100

    added analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import App from "./App/App";
 import store from "./App/store/store";
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
+import {createBrowserHistory} from 'history';
 
+function initializeReactGA() {
+  ReactGA.initialize('UA-136203956-1');
+}
+var history = createBrowserHistory();
 render(
-  <BrowserRouter>
+  <Router history={history}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
+
+initializeReactGA();
+history.listen(analyticsListener);
+
+function analyticsListener(location){
+  console.log("analyticsListener", location);
+  ReactGA.pageview(location.pathname + location.search);
+}
 registerServiceWorker();
