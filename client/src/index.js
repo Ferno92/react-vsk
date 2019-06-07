b6359a22ade3e5f80b871207745503a0f0c170e3
Luca Fernandez <luca.fernandez@spot.it>
2019-06-07 15:19:47 +0200

    second sw

--- client/src/index.js
+++ client/src/index.js
@@ -22,6 +22,17 @@ render(
   </Router>,
   document.getElementById("root")
 );
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./../public/firebase-messaging-sw.js')
+    .then(function (registration) {
+      // firebase.messaging().useServiceWorker(registration);
+      // initializePush(messaging)
+      console.log("firebase-messaging-sw: This service worker is registered")
+    }).catch(function (err) {
+      console.log('firebase-messaging-sw: Service worker registration failed, error:', err);
+    });
+}
 if(window.location.hostname === 'react-vsk.herokuapp.com'){
   initializeReactGA();
   history.listen(analyticsListener);
